328ea52291f176a95c964e6fa2e5ea2e4c0b6268
sehrschlechtYT <66412605+sehrschlechtYT@users.noreply.github.com>
2021-12-05 21:24:40 +0100

    Better tab completion

--- src/main/java/yt/sehrschlecht/hideitem/commands/TabComplete.java
+++ src/main/java/yt/sehrschlecht/hideitem/commands/TabComplete.java
@@ -1,13 +1,15 @@
 package yt.sehrschlecht.hideitem.commands;
 
-import yt.sehrschlecht.hideitem.HideItem;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.jetbrains.annotations.NotNull;
+import yt.sehrschlecht.hideitem.HideItem;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TabComplete implements TabCompleter {
     private HideItem plugin;
@@ -17,21 +19,17 @@ public class TabComplete implements TabCompleter {
     }
 
     @Override
-    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, String[] args) {
         if (args.length >= 1) {
             if (command.getName().equalsIgnoreCase("hideitem")) {
-
                 if (sender.hasPermission("hideitem.reload")) {
                     if (!plugin.getHideItemConfig().DISABLE_COMMANDS())
-                        return Arrays.asList("info", "reload", "toggle", "show", "hide");
-
-                    return Arrays.asList("info", "reload");
+                        return Stream.of("info", "reload", "toggle", "show", "hide").filter(s -> s.startsWith(args[0].toLowerCase(Locale.ENGLISH))).collect(Collectors.toList());
+                    return Stream.of("info", "reload").filter(s -> s.startsWith(args[0].toLowerCase(Locale.ENGLISH))).collect(Collectors.toList());
                 } else {
                     if (!plugin.getHideItemConfig().DISABLE_COMMANDS())
-                        return Arrays.asList("info", "toggle", "show", "hide");
-
-                    return Collections.singletonList("info");
+                        return Stream.of("info", "toggle", "show", "hide").filter(s -> s.startsWith(args[0].toLowerCase(Locale.ENGLISH))).collect(Collectors.toList());;
+                    return Stream.of("info").filter(s -> s.startsWith(args[0].toLowerCase(Locale.ENGLISH))).collect(Collectors.toList());
                 }
             }
         }
